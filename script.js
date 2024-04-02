184c6928e523c0143d90f5fd130312befbb6bf7d
ÐVitaly Fatkullin <vitalyasatka@gmail.com>
2024-04-02 09:36:17 +0500

    edit

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-let array, logo, myElements, analyser, src, height;
-let linesVisualizer = document.getElementById('lines');
-let num = 64;
 let width = 12;
+let spaceBetween = 12;
 let color = '#000000';
+let background = '#ffffff';
+let num = calcNum();
+const skip = num / 4;
 
-const freqElement = document.getElementById('freq');
 const widthElement = document.getElementById('lineWidth');
+const spaceBetweenElement = document.getElementById('spaceBetween');
 const colorElement = document.getElementById('color');
-
-freqElement.addEventListener('change', function ()  {
-    if (+this.value < 8) {
-        this.value = 8;
-    }
-    num = +this.value;
-});
+const backgroundElement = document.getElementById('background');
 
 widthElement.addEventListener('change', function ()  {
     if (+this.value < 1) {
         this.value = 1;
     }
     width = +this.value;
+    num = calcNum();
+});
+
+spaceBetweenElement.addEventListener('change', function ()  {
+    if (+this.value < 1) {
+        this.value = 1;
+    }
+    spaceBetween = +this.value;
+    num = calcNum();
 });
 
 colorElement.addEventListener('change', function ()  {
     color = this.value;
+});
+
+backgroundElement.addEventListener('change', function ()  {
+    document.body.style.backgroundColor = this.value;
+    background = this.value;
 });
 
 const selectsWrapper = document.querySelector('.selects-wrapper');
@@ -41,71 +50,88 @@ const createMedia = async (devicesElement) => {
               option.value = device.deviceId;
               devicesElement.appendChild(option);
           });
-    } catch (error) {
-        alert(error + '\r\n\ Отклонено. Страница будет обновлена!');
-        location.reload();
+    } catch(error) {
+        console.error(error);
     }
 };
 
 selectsElements.forEach(async (selectsElement) => {
-    const typeElement = selectsElement.querySelector('.type');
     const devicesElement = selectsElement.querySelector('.devices');
 
     await createMedia(devicesElement);
-
-    devicesElement.addEventListener('change', function () {
-        const context = new AudioContext();
-        analyser = context.createAnalyser();
-        analyser.fftSize = num*4;
-        const bufferLength = analyser.frequencyBinCount;
-        array = new Uint8Array(bufferLength);
-
-        navigator.mediaDevices.getUserMedia({
-            audio: { deviceId: this.value }
-        }).then(stream => {
-            src = context.createMediaStreamSource(stream);
-            src.connect(analyser);
-            if (typeElement.value === 'lines') {
-                linesVisualizer.style.display = 'flex';
-                visualizeLines();
-            }
-            if (typeElement.value === 'waves') {
-                canvasElement.style.display = 'block';
-                visualizeWaves();
-            }
-        }).catch(error => {
-            alert(error + '\r\n\ Отклонено. Страница будет обновлена!');
-            location.reload();
-        });
-
-        selectsWrapper.remove();
-    });
 });
 
-function visualizeLines() {
+const startButton = document.getElementById('start');
+startButton.onclick = start;
+
+function start() {
+    selectsElements.forEach(async (selectsElement) => {
+        const type = selectsElement.querySelector('.type').value;
+        const device = selectsElement.querySelector('.devices').value;
+
+        const context = new AudioContext();
+        const analyser = context.createAnalyser();
+        analyser.fftSize = num*4;
+        const bufferLength = analyser.frequencyBinCount;
+        const array = new Uint8Array(bufferLength);
+
+        navigator.mediaDevices.getUserMedia({
+            audio: { deviceId: device }
+        }).then(stream => {
+            const src = context.createMediaStreamSource(stream);
+            src.connect(analyser);
+            if (type === 'lines') {
+                visualizeLines(selectsElement.dataset.id, analyser, array);
+            }
+            if (type === 'rectangles') {
+                visualizeLines(selectsElement.dataset.id, analyser, array, true);
+            }
+            if (type === 'waves') {
+                visualizeWaves(selectsElement.dataset.id, analyser, array);
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+    });
+
+    selectsWrapper.remove();
+}
+
+function visualizeLines(id, analyser, array, rectangle = false) {
+    const visualizersWrappers = document.querySelectorAll('.visualizers-wrapper');
+    const visualizer = visualizersWrappers[id].querySelector('.visualizer_lines');
+
     for(let i = 0 ; i < num ; i++){
-        logo = document.createElement('div');
+        const logo = document.createElement('div');
         logo.className = 'logo';
-        logo.style.background = color;
-        logo.style.minWidth = width+'px';
-        logo.style.margin = `0 ${width / 2}px`;
-        linesVisualizer.appendChild(logo);
+
+        logo.style.border = `${width / 2}px solid ${color}`;
+        logo.style.margin = `${spaceBetween / 2}px 0`;
+
+        if (rectangle) {
+            logo.style.border = `${width}px solid ${color}`;
+            logo.style.padding = width / 2 + 'px';
+        }
+
+        visualizer.appendChild(logo);
     }
-    myElements = document.getElementsByClassName('logo');
+
+    const logos = visualizer.getElementsByClassName('logo');
     draw();
 
     function draw() {
         window.requestAnimationFrame(draw);
         analyser.getByteFrequencyData(array);
         for(let i = 0 ; i < num ; i++){
-            height = array[i];
-            myElements[i].style.minHeight = height+'px';
+            logos[i].style.width = array[i+skip] * 2 + 'px';
         }
     }
+
+    visualizer.style.display = 'flex';
 }
 
-function visualizeWaves() {
-    const canvasElement = document.getElementById('waves');
+function visualizeWaves(id, analyser, array) {
+    const canvasElement = document.querySelectorAll('.visualizer_waves')[id];
     const canvasCtx = canvasElement.getContext("2d");
     draw();
 
@@ -116,36 +142,52 @@ function visualizeWaves() {
         requestAnimationFrame(draw);
         analyser.getByteFrequencyData(array);
 
-        canvasCtx.fillStyle = 'rgb(255, 255, 255)';
+        canvasCtx.fillStyle = background;
         canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
+        const slice = spaceBetween + width;
+        const halfSlice = slice / 2;
         canvasCtx.lineWidth = width;
         canvasCtx.strokeStyle = color;
 
         canvasCtx.beginPath();
 
-        let sliceWidth = WIDTH * 1.0 / num;
-        let x = 0;
+        let y = 0;
 
 
         for(let i = 0; i < num; i++) {
-            const freq = array[i];
-            const halfSlice = sliceWidth / 2;
+            const freq = array[i+skip];
 
-            let y =  i % 2 === 0 ? HEIGHT / 2 + freq : HEIGHT / 2 - freq;
+            let x =  i % 2 === 0 ? WIDTH / 2 + freq : WIDTH / 2 - freq;
 
             if(i === 0) {
                 canvasCtx.moveTo(x, y);
             } else {
-                canvasCtx.quadraticCurveTo(x - halfSlice, y, x, y);
+                canvasCtx.quadraticCurveTo(x, y - halfSlice, x, y);
             }
 
-            canvasCtx.quadraticCurveTo(x + halfSlice, y, x + halfSlice, HEIGHT / 2);
+            canvasCtx.quadraticCurveTo(x, y + halfSlice, WIDTH / 2, y + halfSlice);
 
-            x += sliceWidth;
+            y += slice;
         }
 
         canvasCtx.stroke();
 
     }
+
+    canvasElement.style.display = 'flex';
+}
+
+function aroundSquareTwo(number) {
+    let result = 2;
+
+    while (result < number) {
+        result *= 2;
+    }
+
+    return result;
+}
+
+function calcNum() {
+    return aroundSquareTwo(2196 / (width + spaceBetween));
 }
